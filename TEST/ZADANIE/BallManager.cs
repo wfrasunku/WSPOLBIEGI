990fd7b9f593d5dd24adc89ce8469e00f1f350d8
X-ray160 <patrykaugustyniak020703@gmail.com>
2024-04-14 10:08:02 +0200

    Wstępna warstwa danych i logiki (?)
    zrobiona wstępna warstwa danych Balls i logiki BallManager(choć rozmiar stołu to dane więc może nie powinny być w BallManager?), i do tych rzeczy jakieś testy, wszystkie przechodzące poprawnie

---
+++ TEST/ZADANIE/BallManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ZADANIE;
+
+public class BallManager
+{
+    public ObservableCollection<Ball> balls { get; private set; }
+    private const int TableWidth = 1000; // Szerokość obszaru stołu
+    private const int TableHeight = 600; // Wysokość obszaru stołu
+
+    public BallManager()
+    {
+        balls = new ObservableCollection<Ball>();
+    }
+
+    // Metoda do dodawania nowej kuli
+    public void AddBall(Ball ball)
+    {
+        balls.Add(ball);
+    }
+
+    // Metoda do aktualizacji położenia wszystkich kul
+    public void UpdateBalls()
+    {
+        List<Ball> ballsToRemove = new List<Ball>();
+
+        foreach (var ball in balls)
+        {
+            ball.Move(); // Aktualizacja położenia każdej kuli na podstawie jej prędkości
+
+            // Sprawdzenie kolizji z krawędziami stołu
+            if (ball.X - ball.Radius < 0 || ball.X + ball.Radius > TableWidth ||
+                ball.Y - ball.Radius < 0 || ball.Y + ball.Radius > TableHeight)
+            {
+                ballsToRemove.Add(ball); // Dodaj kule do listy do usunięcia
+            }
+        }
+
+        // Usuń kule, które opuściły obszar stołu
+        foreach (var ballToRemove in ballsToRemove)
+        {
+            balls.Remove(ballToRemove);
+        }
+    }
+
+    // Metoda do pobierania aktualnej liczby kul
+    public int GetBallCount()
+    {
+        return balls.Count;
+    }
+}
+
---
+++ TEST/ZADANIE_TEST/ModelTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZADANIE;
+
+namespace ZADANIE_TEST
+{
+    [TestClass]
+    public class BallTests
+    {
+        [TestMethod]
+        public void Ball_Move_ShouldUpdatePosition()
+        {
+            // Arrange
+            Ball ball = new Ball(0, 0, 10, 1, 1);
+            double expectedX = 1;
+            double expectedY = 1;
+
+            // Act
+            ball.Move();
+
+            // Assert
+            Assert.AreEqual(expectedX, ball.X);
+            Assert.AreEqual(expectedY, ball.Y);
+        }
+    }
+
+    [TestClass]
+    public class BallManagerTests
+    {
+        [TestMethod]
+        public void BallManager_AddBall_ShouldAddBallToList()
+        {
+            // Arrange
+            BallManager ballManager = new BallManager();
+            Ball ball = new Ball(0, 0, 10, 1, 1);
+
+            // Act
+            ballManager.AddBall(ball);
+
+            // Assert
+            Assert.AreEqual(1, ballManager.GetBallCount());
+            CollectionAssert.Contains(ballManager.balls, ball);
+        }
+
+        [TestMethod]
+        public void BallManager_UpdateBalls_ShouldRemoveBallsOutsideTable()
+        {
+            // Arrange
+            BallManager ballManager = new BallManager();
+            Ball ballInside = new Ball(50, 50, 10, 1, 1);
+            Ball ballOutside = new Ball(1050, 1050, 10, 1, 1);
+            ballManager.AddBall(ballInside);
+            ballManager.AddBall(ballOutside);
+
+            // Act
+            ballManager.UpdateBalls();
+
+            // Assert
+            Assert.AreEqual(1, ballManager.GetBallCount());
+            CollectionAssert.Contains(ballManager.balls, ballInside);
+            CollectionAssert.DoesNotContain(ballManager.balls, ballOutside);
+        }
+    }
+}
