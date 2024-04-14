e71d1b5eec5fe5d144df9cfa54203c4926a2832e
X-ray160 <patrykaugustyniak020703@gmail.com>
2024-04-14 10:11:51 +0200

    małe czyszczenie

--- TEST/ZADANIE_TEST/ModelTests.cs
+++ TEST/ZADANIE_TEST/ModelTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZADANIE;
+﻿using ZADANIE;
 
 namespace ZADANIE_TEST
 {
@@ -13,15 +8,12 @@ namespace ZADANIE_TEST
         [TestMethod]
         public void Ball_Move_ShouldUpdatePosition()
         {
-            // Arrange
             Ball ball = new Ball(0, 0, 10, 1, 1);
             double expectedX = 1;
             double expectedY = 1;
 
-            // Act
             ball.Move();
 
-            // Assert
             Assert.AreEqual(expectedX, ball.X);
             Assert.AreEqual(expectedY, ball.Y);
         }
@@ -33,14 +25,11 @@ namespace ZADANIE_TEST
         [TestMethod]
         public void BallManager_AddBall_ShouldAddBallToList()
         {
-            // Arrange
             BallManager ballManager = new BallManager();
             Ball ball = new Ball(0, 0, 10, 1, 1);
 
-            // Act
             ballManager.AddBall(ball);
 
-            // Assert
             Assert.AreEqual(1, ballManager.GetBallCount());
             CollectionAssert.Contains(ballManager.balls, ball);
         }
@@ -48,17 +37,14 @@ namespace ZADANIE_TEST
         [TestMethod]
         public void BallManager_UpdateBalls_ShouldRemoveBallsOutsideTable()
         {
-            // Arrange
             BallManager ballManager = new BallManager();
             Ball ballInside = new Ball(50, 50, 10, 1, 1);
             Ball ballOutside = new Ball(1050, 1050, 10, 1, 1);
             ballManager.AddBall(ballInside);
             ballManager.AddBall(ballOutside);
 
-            // Act
             ballManager.UpdateBalls();
 
-            // Assert
             Assert.AreEqual(1, ballManager.GetBallCount());
             CollectionAssert.Contains(ballManager.balls, ballInside);
             CollectionAssert.DoesNotContain(ballManager.balls, ballOutside);
