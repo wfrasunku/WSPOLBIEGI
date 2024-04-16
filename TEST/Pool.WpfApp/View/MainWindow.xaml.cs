1069af1ae10785033eba5ef60c2858f8750bae20
wfrasunku <afrasun@wp.pl>
2024-04-16 13:24:55 +0200

    Adding WPF

---
+++ TEST/Pool.WpfApp/View/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Pool.WpfApp.ViewModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pool.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
