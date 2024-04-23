0bc7e14f03e24bcf8ae60370112aa34455ea5650
wfrasunku <afrasun@wp.pl>
2024-04-23 08:29:55 +0200

    FIX to Movement

--- ConcurrentProgramming/PresentationModel/ModelBall.cs
+++ ConcurrentProgramming/PresentationModel/ModelBall.cs
@@ -11,7 +11,7 @@ namespace TP.ConcurrentProgramming.PresentationModel
     {
       TopBackingField = top;
       LeftBackingField = left;
-      MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
+      MoveTimer = new Timer(Move, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(50));
     }
 
     #region IBall
@@ -76,20 +76,16 @@ namespace TP.ConcurrentProgramming.PresentationModel
     {
       if (state != null)
         throw new ArgumentOutOfRangeException(nameof(state));
-      double newTop = Top + (Random.NextDouble() - 0.5) * 5;
-      double newLeft = Left + (Random.NextDouble() - 0.5) * 15;
+      double randTop = (Random.NextDouble() - 0.5) * 10;
+      Top = Top + randTop;
+      double randLeft = (Random.NextDouble() - 0.5) * 25;
+      Left = Left + randLeft;
 
-            if (newTop < 5 || newTop > 395)
-                newTop = Top * -1 * (Random.NextDouble() - 0.5) * 5;
+            if (Top < 0 || Top > 360)
+                Top = Top - randTop;
 
-            if (newLeft < 5 || newLeft > 795)
-                newLeft = Left * -1 * (Random.NextDouble() - 0.5) * 15;
-
-            if (newTop >= 0 && newTop <= 400 && newLeft >= 0 && newLeft <= 800)
-            {
-                Top = newTop;
-                Left = newLeft;
-            }
+            if (Left < 0 || Left > 760)
+                Left = Left - randLeft;
         }
 
     #endregion private
