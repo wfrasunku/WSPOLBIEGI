30d33fa696adb67191ee7dd365a9fcf5b595cdca
X-ray160 <patrykaugustyniak020703@gmail.com>
2024-04-24 00:09:33 +0200

    testowanie kul

--- ConcurrentProgramming/PresentationViewModelTest/UnitTest1.cs
+++ ConcurrentProgramming/PresentationViewModelTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using TP.ConcurrentProgramming.PresentationViewModel;
 
 namespace PresentationViewModelTest;
@@ -10,4 +11,25 @@ public class UnitTest1
         MainWindowViewModel window = new MainWindowViewModel();
         Assert.IsNotNull(window.Balls);
     }
+
+    [Test]
+    public void AddBallsTest()
+    {
+        MainWindowViewModel window = new MainWindowViewModel();
+        window.AddBallCommand.Execute(null);
+        window.AddBallCommand.Execute(null);
+        Assert.AreEqual(2, window.Balls.Count);
+    }
+
+    [Test]
+    public void RemoveBallsTest()
+    {
+        MainWindowViewModel window = new MainWindowViewModel();
+        window.AddBallCommand.Execute(null);
+        window.AddBallCommand.Execute(null);
+        Assert.AreEqual(2, window.Balls.Count);
+        window.RemoveBallCommand.Execute(null);
+        Assert.AreEqual(1, window.Balls.Count);
+    }
+
 }
