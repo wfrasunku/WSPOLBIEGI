dfba91095aa063f4d141c158fece0a3a8b8ba52d
wfrasunku <afrasun@wp.pl>
2024-05-08 16:26:26 +0200

    GenerateBalls added

---
+++ Pool/Pool/ViewModel/GenerateBalls.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Model;
+
+namespace ViewModel
+{
+    public class GenerateBalls : INotifyPropertyChanged
+    {
+        private ObservableCollection<Ball> balls = new();
+        private AbstractModelApi api;
+
+        public RelayCommand AddBallCommand => new RelayCommand(execute => api.AddBall());
+        public RelayCommand RemoveBallCommand => new RelayCommand(execute => api.RemoveBall(), canExecute => Balls != null);
+        public RelayCommand StartMoveCommand => new RelayCommand(
+            execute =>
+            {
+                String str = execute as String;
+                if (uint.TryParse(str, out uint value)) { this.api.Start((int)value); }
+                this.Balls = api.GetBalls();
+            });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public GenerateBalls() => this.api = AbstractModelApi.API();
+
+        public ObservableCollection<Ball> Balls
+        {
+            get { return balls; }
+            set
+            {
+                if (balls != value)
+                {
+                    balls = value;
+                    OnPropertyChanged(nameof(balls));
+                }
+            }
+        }
+    }
+
+}
---
+++ Pool/Pool/ViewModel/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null || canExecute(parameter);
+
+        public void Execute(object parameter) => execute(parameter);
+    }
+}
+
