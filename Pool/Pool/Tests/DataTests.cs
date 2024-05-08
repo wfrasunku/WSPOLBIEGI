2d42c7835294a1cfa79e5cc7f1b3f109e6069bd4
wfrasunku <afrasun@wp.pl>
2024-05-08 16:48:25 +0200

    BallTests

---
+++ Pool/Pool/Tests/DataTests.cs
@@ -0,0 +1,36 @@
+using Data;
+
+namespace Tests
+{
+    public class BallTest
+    {
+        BallData ball = new BallData(30, 40, 20);
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetterTest()
+        {
+            Assert.AreEqual(30, ball.X);
+            Assert.AreEqual(40, ball.Y);
+            Assert.AreEqual(20, ball.Diameter);
+        }
+
+        [Test]
+        public void SetterTest()
+        {
+            ball.SetSpeed(2, 3);
+            ball.X = 50;
+            ball.Y = 60;
+            ball.Diameter = 30;
+
+            Assert.AreEqual(2, ball.XSpeed);
+            Assert.AreEqual(3, ball.YSpeed);
+            Assert.AreEqual(50, ball.X);
+            Assert.AreEqual(60, ball.Y);
+            Assert.AreEqual(30, ball.Diameter);
+        }
+    }
+}
