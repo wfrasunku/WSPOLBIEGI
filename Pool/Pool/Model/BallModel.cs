a1f0d6b629d17435f30cb9b914152681b3913a54
wfrasunku <afrasun@wp.pl>
2024-05-08 16:28:51 +0200

    Model added

---
+++ Pool/Pool/Model/BallModel.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using Logic;
+
+namespace Model
+{
+    public class BallModel : INotifyPropertyChanged
+    {
+        private double x;
+        private double y;
+        private double diameter;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public double X
+        {
+            get { return x; }
+            set
+            {
+                if (x != value)
+                {
+                    x = value;
+                    OnPropertyChanged(nameof(X));
+                }
+            }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set
+            {
+                if (y != value)
+                {
+                    y = value;
+                    OnPropertyChanged(nameof(Y));
+                }
+            }
+        }
+
+        public double Diameter
+        {
+            get { return diameter; }
+            set
+            {
+                if (diameter != value)
+                {
+                    diameter = value;
+                    OnPropertyChanged(nameof(Diameter));
+                }
+            }
+        }
+
+        public BallModel(BallLogic ball)
+        {
+            this.x = ball.X;
+            this.y = ball.Y;
+            this.diameter = ball.Diameter;
+            ball.PropertyChanged += Move;
+        }
+
+        private void Move(object sender, PropertyChangedEventArgs e)
+        {
+            BallLogic ball = (BallLogic)sender;
+            if (e.PropertyName == nameof(ball.X))
+            {
+                this.X = ball.X;
+
+            }
+            if (e.PropertyName == nameof(ball.Y))
+            { this.Y = ball.Y; }
+        }
+    }
+}
---
+++ Pool/Pool/Model/ModelApi.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using Logic;
+
+namespace Model
+{
+    public abstract class AbstractModelApi
+    {
+        public static AbstractModelApi API(AbstractLogicApi abstractLogicApi = null) => new ModelApi();
+
+        public abstract void Start(int amountOfBalls);
+        public abstract void AddBall();
+        public abstract void RemoveBall();
+        public abstract ObservableCollection<BallModel> GetBalls();
+
+
+        public class ModelApi : AbstractModelApi
+        {
+            private ObservableCollection<BallModel> balls = new();
+            private AbstractLogicApi logicApi = AbstractLogicApi.API(null);
+
+            public ModelApi(AbstractLogicApi abstractLogicApi = null)
+            {
+                if (abstractLogicApi == null)
+                {
+                    this.logicApi = AbstractLogicApi.API();
+                }
+                else
+                {
+                    this.logicApi = abstractLogicApi;
+                }
+            }
+
+            public ObservableCollection<BallModel> Balls
+            {
+                get { return balls; }
+                set { balls = value; }
+            }
+
+            public override void Start(int amountOfBalls) => logicApi.StartUpdating(amountOfBalls);
+
+            public override ObservableCollection<BallModel> GetBalls()
+            {
+                List<BallLogic> logicBalls = logicApi.GetBalls();
+                Balls.Clear();
+                foreach (BallLogic ball in logicBalls)
+                {
+                    Balls.Add(new BallModel(ball));
+                }
+                return Balls;
+            }
+
+            public override void AddBall() => logicApi.AddBall();
+
+            public override void RemoveBall() => logicApi.RemoveBall();
+        }
+    }
+}
